bf8a1511820fc15a965e0c5c2f1720c7a72c77b6
takahasi6661 <alexa.ilyaschenko@yandex.ru>
2020-09-30 13:00:04 +0300

    проверка операции и нахождение суммы, если операция это +

--- venv/calculator.js
+++ venv/calculator.js
@@ -1,3 +1,6 @@
-let a=prompt('Введите первое число')
-let b=prompt('Введите второе число')
-let c=prompt('Введите операцию')
+let a=prompt('Введите первое число');
+let b=prompt('Введите второе число');
+let c=prompt('Введите операцию');
+if (c=='+') {
+    alert(Number(a)+Number(b));
+}
