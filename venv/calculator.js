0727824fb42b37e862770bf9fbf3f52f29dc3138
takahasi6661 <alexa.ilyaschenko@yandex.ru>
2020-09-30 13:09:29 +0300

    проверка операции и смена знака, если операция это смена знака, и подробное описание операций

--- venv/calculator.js
+++ venv/calculator.js
@@ -1,12 +1,15 @@
 let a=prompt('Введите первое число');
 let b=prompt('Введите второе число');
-let c=prompt('Введите операцию');
+let c=prompt('Введите операцию:-,+,*,/, смена знака');
 if (c=='+') {
     alert(Number(a)+Number(b));
 }  else if(c=='-'){
-    alert(a-b)
+    alert(a-b);
 } else if(c=='*'){
-    alert(a*b)
+    alert(a*b);
 } else if(c=='/'){
-    alert(a/b)
+    alert(a/b);
+} else if(c=='смена знака'){
+    alert(-a);
+    alert(-b);
 }
